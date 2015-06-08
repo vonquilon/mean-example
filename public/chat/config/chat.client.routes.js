3aedcf6ab663bf0edbe8ec87e3ad3fc07d329d55
vonquilon <von.quilon@gmail.com>
2015-06-08 07:18:22 -0400

    first commit

---
+++ public/chat/config/chat.client.routes.js
@@ -0,0 +1,5 @@
+angular.module('chat').config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/chat', {
+        templateUrl: 'chat/views/chat.client.view.html'
+    });
+}]);
